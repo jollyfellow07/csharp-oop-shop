1d2058d91c14c9d2de523017e3392c718e0bceb0
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-08 17:13:36 +0200

    aggiunti 3 prodotti

--- Csharp Shop/Prodotto.cs
+++ Csharp Shop/Prodotto.cs
@@ -16,30 +16,66 @@ namespace Csharp_Shop
 
         public Prodotto(string name, string descrizione, double prezzo, double iva)
         {
-            
+
             this.name = name;
             this.descrizione = descrizione;
             this.prezzo = prezzo;
             this.iva = iva;
-        }
-        
-        public int GetCodice()
-        {
             
-            codice = numeroRandomCodice();
-            Console.Write("il codice prodotto è: " + codice);
-            return codice;
-        }
-        public string GetDescrizione()
-        { 
-            return descrizione;
         }
 
-        public int numeroRandomCodice()
+        public double GetPrezzo()
         { 
+            return prezzo;
+        }
+
+        public int GetCodice()
+        {
+
+            codice = numeroRandomCodice();
+            return codice;
+        }
+     
+
+        public int numeroRandomCodice()
+        {
             Random codiceRandom = new Random();
             return (int)codiceRandom.Next(50);
         }
+        public void stampa()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n**************"+ codice+ "-" + name +  "***************");
+            Console.WriteLine("Il nome del prodotto è: " + name);
+            Console.WriteLine("il codice è: " + codice );
+            Console.WriteLine("il prezzo senza iva è: " +  prezzo + "€");
+            Console.WriteLine("L'iva del prodotto è: " + iva + "%");
+            Console.WriteLine("Il prezzo del prodotto con iva è: " + CalcoloPrezzoConIva() + "€");
+            Console.WriteLine("\n**************************************");
+        }
+
+        public double CalcoloPrezzoConIva()
+        {
+            double prezzoFinale;
+            if (iva == 22)
+            {
+                prezzoFinale = prezzo * 1.22;
+               
+            }
+            else if (iva == 5)
+            {
+                prezzoFinale = prezzo * 1.05;
+                
+            }  else
+            {
+                prezzoFinale = prezzo * 1.10;
+            }
+           prezzoFinale = Math.Round(prezzoFinale, 2);         
+            return prezzoFinale;
+        }
+        
+
+            
              
     }
 }
--- Csharp Shop/Program.cs
+++ Csharp Shop/Program.cs
@@ -2,7 +2,14 @@
 using Csharp_Shop;
 
 
-Prodotto mela = new Prodotto("si chiama melinda", "questa mela è rossa",4,6 ) ;
-
+Prodotto mela = new Prodotto("MELA", "questa mela è rossa",0.50, 5);
 int codice = mela.GetCodice();
+mela.stampa();
 
+Prodotto telefono = new Prodotto("iphone", "Apple iPhone 13 è l’ultima uscita di casa Apple, apparentemente simile al suo predecessore, ma in realtà molto innovativo per display, batteria e fotocamera. ", 1000, 22);
+int iphoneCodice = telefono.GetCodice();
+telefono.stampa();
+
+Prodotto medicina = new Prodotto("tachipirina", "questa mela è rossa", 10.20, 10);
+int medicinaCodice = medicina.GetCodice();
+medicina.stampa();
