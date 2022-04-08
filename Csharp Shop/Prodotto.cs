51f80379e9342abf28a1c6c93946cfc5e4f81ba2
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-08 15:45:24 +0200

    creato metodo per assegnare con codice prodotto

---
+++ Csharp Shop/Prodotto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Shop
+{
+    internal class Prodotto
+    {
+        private int codice;
+        private string name;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+        public Prodotto(string name, string descrizione, double prezzo, double iva)
+        {
+            
+            this.name = name;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        
+        public int GetCodice()
+        {
+            
+            codice = numeroRandomCodice();
+            Console.Write("il codice prodotto è: " + codice);
+            return codice;
+        }
+        public string GetDescrizione()
+        { 
+            return descrizione;
+        }
+
+        public int numeroRandomCodice()
+        { 
+            Random codiceRandom = new Random();
+            return (int)codiceRandom.Next(50);
+        }
+             
+    }
+}
--- Csharp Shop/Program.cs
+++ Csharp Shop/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Csharp_Shop;
+
+
+Prodotto mela = new Prodotto("si chiama melinda", "questa mela è rossa",4,6 ) ;
+
+int codice = mela.GetCodice();
+
